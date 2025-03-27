fa52163f6a57a2aa906b3752808ea434a900e3f3
DenisG30 <gorbunowdenis30@gmail.com>
2025-03-27 20:34:49 +0400

    Update Level1.java

--- Level 1.7/Level1.java
+++ Level 1.7/Level1.java
@@ -1,89 +1,58 @@
 public class Level1 {
-    public static int [] WordSearch(int len, String s, String subs) {
-        String [] words = s.split("\s+");
-        String nStr = "";
-        int itemCount = len;
-
-        for(int i = 0; i < words.length; i++) {
-            if(words[i] != "") {
-                if(words[i].length() <= itemCount) {
-                    nStr += words[i];
-                    itemCount -= words[i].length();
-                    if(i + 1 < words.length) {
-                        if(words[i + 1].length() <= itemCount - 1) {
-                            nStr += " ";
-                            itemCount -= 1;
-                        } else {
-                            nStr += "\n";
-                            itemCount = len;
-                        }
-                    }
-                } else {
-                    int iteration = 0;
-                    if(words[i].length() % len != 0 ){
-                        iteration = words[i].length() / len + 1;
-                    } else {
-                        iteration = words[i].length() / len;
-                    }
-                    int countTransfer = 1;
-                    int nItemCount = 0;
-                    String nWord = ""; 
-                    for(int j = 0; j < iteration; j++) {
-                        if(j != iteration - 1) {
-                            nWord = words[i].substring(nItemCount, countTransfer * len);
-                            nStr += nWord;
-                            nStr += "\n";
-                            nItemCount += len;
-                        } else {
-                            nWord = words[i].substring(words[i].length() - (words[i].length() % len));
-                            nStr += nWord;
-                            itemCount -= nWord.length();
-                            if(i + 1 < words.length) {
-                                if(words[i + 1].length() <= itemCount) {
-                                    nStr += " ";
-                                    itemCount -= 1;
-                                } else {
-                                    nStr += "\n";
-                                    itemCount = len;
-                                }
-                            }
-                        }   
-                        countTransfer ++;
-                    }
+    public static int[]  WordSearch(int len, String s, String subs) {
+        String[] words = s.split(" ");
         
+        StringBuilder lineInNstr = new StringBuilder();
+        StringBuilder nStr = new StringBuilder();
+
+        for (String word : words) {
+            if (word.length() > len) {
+                if (lineInNstr.length() > 0) {
+                    nStr.append(lineInNstr.toString()).append("\n");
+                    lineInNstr.setLength(0);
+                }
+                for (int i = 0; i < word.length(); i += len) {
+                    int end = Math.min(i + len, word.length());
+                    nStr.append(word.substring(i, end)).append("\n");
+                }
+            } else {
+                if (lineInNstr.length() + word.length() + 1 <= len) {
+                    if (lineInNstr.length() > 0) {
+                        lineInNstr.append(" ");
+                    }
+                    lineInNstr.append(word);
+                } else {
+                    nStr.append(lineInNstr.toString()).append("\n");
+                    lineInNstr.setLength(0);
+                    lineInNstr.append(word);
                 }
             }
         }
+        if (lineInNstr.length() > 0) {
+            nStr.append(lineInNstr.toString());
+        }
         
-        words = nStr.split("\\n");
+        words = nStr.toString().split("\n");
+        
         int[] res = new int[words.length];
-        int subsLength = subs.length();
-
+        
         for(int i = 0; i < words.length; i++){
-            int wordLength = words[i].length();  
-            if(wordLength < subsLength) {
-                res[i] = 0;
-                continue;
-            }     
+            boolean answ = false;
+            String str = words[i];
+            String[] strWords = str.split(" ");
             
-            for (int p = 0; p < wordLength; p++) {
-                int j;
-                for (j = 0; j < subsLength; j++) {
-                    if (words[i].charAt(p + j) != subs.charAt(j)) {
-                        if(p < wordLength)
-                        break; 
-                    }
-                }
-                
-                if (j == subsLength) {
-                    boolean startVal = (p == 0 || words[i].charAt(p - 1) == ' ');
-                    boolean endVal = (p + subsLength == wordLength || words[i].charAt(p + subsLength) == ' ');
-                    if(startVal && endVal){
-                        res[i] = 1;
-                    }
+            for(String word : strWords) {
+                if(word.equals(subs)) {
+                    answ = true;
+                    break;
                 }
             }
-        } 
+            if(answ) {
+                res[i] = 1;
+            } else { 
+                res[i] = 0;
+            }
+        }
         return res;
     }
 }
