95ad3fa38eb4a7a26196a608edcfbd53faea9224
DenisG30 <gorbunowdenis30@gmail.com>
2025-05-22 10:23:49 +0400

    Update Level1.java

--- Level 1.23/Level1.java
+++ Level 1.23/Level1.java
@@ -1,20 +1,35 @@
 public class Level1 {
     public static String[] TreeOfLife(int H, int W, int N, String[] tree) {
         char[][] grid = new char[H][W]; 
+        int[][] gridInt = new int[H][W]; 
         
         for (int i = 0; i < H; i++) {
             grid[i] = tree[i].toCharArray();
-        }     
+        }
+        
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                if(grid[i][j] == '.') {
+                    gridInt[i][j] = 0;
+                }
+
+                if(grid[i][j] == '+') {
+                    gridInt[i][j] = 1;
+                }
+            }
+        }    
 
         for (int year = 1; year <= N; year++) {
-            grid = age(grid); 
-            if (year % 2 == 1) { 
-                grid = delete(grid); 
-            } else { 
-                grid = fillCells(grid); 
+            gridInt = age(gridInt); 
+            if(year % 2 == 0) {
+                gridInt = delete(gridInt);
+            } else {
+                gridInt = fillCells(gridInt);
             }
         }
-       
+        
+        grid = toChar(gridInt);
+
         String[] result = new String[H];
         for (int i = 0; i < H; i++) {
             result[i] = new String(grid[i]);
@@ -22,28 +37,24 @@ public class Level1 {
         return result;
     }
 
-    private static char[][] age(char[][] grid) {
-        char[][] newGrid = new char[grid.length][grid[0].length];
+    private static int[][] age(int[][] grid) {
+        int[][] newGrid = new int[grid.length][grid[0].length];
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] != '.') {
-                    newGrid[i][j] = (char) (grid[i][j] + 1);
-                } else {
-                    newGrid[i][j] = '.';
-                }
+                newGrid[i][j] = grid[i][j] + 1;
             }
         }
         return newGrid;
     }
     
-    private static char[][] fillCells(char[][] grid) {
-        char[][] newGrid = new char[grid.length][grid[0].length];
+    private static int[][] fillCells(int[][] grid) {
+        int[][] newGrid = new int[grid.length][grid[0].length];
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] == '.') {
-                    newGrid[i][j] = '+'; 
+                if (grid[i][j] == 0) {
+                    newGrid[i][j] = 1; 
                 } else {
                     newGrid[i][j] = grid[i][j]; 
                 }
@@ -52,34 +63,33 @@ public class Level1 {
         return newGrid;
     }
     
-    private static char[][] delete(char[][] grid) {
+    private static int[][] delete(int[][] grid) {
         boolean[][] toDelete = new boolean[grid.length][grid[0].length];
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
-                if (grid[i][j] - '0' >= 3) { 
+                if (grid[i][j] >= 3) { 
                     toDelete[i][j] = true;
                     toDelete = markToDelete(toDelete, grid, i, j);
                 }
             }
         }
 
-        char[][] newGrid = new char[grid.length][grid[0].length];
+        int[][] newGrid = new int[grid.length][grid[0].length];
 
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[i].length; j++) {
                 if (toDelete[i][j]) {
-                    newGrid[i][j] = '.';
+                    newGrid[i][j] = 0;
                 } else {
                     newGrid[i][j] = grid[i][j]; 
                 }
             }
         }
-        
         return newGrid;
     }
 
-    private static boolean[][] markToDelete(boolean[][] toDelete, char[][] grid, int x, int y) {
+    private static boolean[][] markToDelete(boolean[][] toDelete, int[][] grid, int x, int y) {
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
 
@@ -92,4 +102,18 @@ public class Level1 {
         }
         return toDelete;
     }
+
+    private static char[][] toChar(int[][] grid) {
+        char[][] newGrid = new char[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if(grid[i][j] == 0) {
+                    newGrid[i][j] = '.';
+                } else {
+                    newGrid[i][j] = '+';
+                }
+            }
+        }
+        return newGrid;
+    }    
 }
