4922e7e23c7bd906cd8530463835ce6cfb4c1eb5
DenisG30 <gorbunowdenis30@gmail.com>
2025-05-31 12:38:26 +0400

    Update Level1.java

--- Level 1.26/Level1.java
+++ Level 1.26/Level1.java
@@ -1,25 +1,36 @@
 public class Level1 {
     public static boolean white_walkers(String village) {
-        for (int i = 0; i < village.length() - 2; i++) {
+        int countInt = 0;
+        for (int i = 0; i < village.length(); i++) {
+            if (Character.isDigit(village.charAt(i))) {
+                countInt++;
+            }
+        }
+        if (countInt < 2 || countInt == 3) {
+            return false;
+        }
+
+        for (int i = 0; i < village.length(); i++) {
             if (Character.isDigit(village.charAt(i))) {
                 int firstNumber = Character.getNumericValue(village.charAt(i));
-                int j = i + 1;
-                int equalCount = 0;
-                for(; j < village.length(); j++) {
-                    if(village.charAt(j) == '=') {
-                        equalCount++;
-                    } else {
-                        break;
-                    }
-                }  
-                if (j < village.length() && Character.isDigit(village.charAt(j))) {
-                    int secondNumber = Character.getNumericValue(village.charAt(j));
-                    if (firstNumber + secondNumber == 10 && equalCount == 3) {
-                        return true; 
+
+                for (int j = i + 1; j < village.length(); j++) {
+                    if (Character.isDigit(village.charAt(j))) {
+                        int secondNumber = Character.getNumericValue(village.charAt(j));
+                        int equalCount = 0;
+                        for (int k = i + 1; k < j; k++) {
+                            if (village.charAt(k) == '=') {
+                                equalCount++;
+                            }
+                        }
+                        if (firstNumber + secondNumber == 10 && equalCount == 3) {
+                            return true;
+                        }
                     }
                 }
+
             }
         }
         return false; 
-    }
-}
+    }        
+} 
