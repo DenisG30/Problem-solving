0233f2235ace8a6f63e227c71e2d51ebd05a95eb
DenisG30 <gorbunowdenis30@gmail.com>
2025-09-20 22:13:25 +0300

    Create Stack.java

---
+++ ASD2/Task4/Stack.java
@@ -0,0 +1,58 @@
+public class Stack<T> {
+    private Node<T> head;
+    private int count;
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        Node(T value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    public Stack() {
+        head = null;
+        count = 0;
+    }
+
+    // Time complexity O(1)
+    // Spatial complexity O(1)
+    public int size() {
+        return count; 
+    }
+
+    // Time complexity O(1)
+    // Spatial complexity O(1)
+    public T pop() {
+        if (head == null) {
+            return null; 
+        }
+        T value = head.value; 
+        head = head.next; 
+        count--; 
+        return value; 
+    }
+
+    // Time complexity O(1)
+    // Spatial complexity O(1)
+    public void push(T val) {
+        Node<T> newNode = new Node<>(val); 
+        newNode.next = head; 
+        head = newNode;
+        count++;
+    }
+
+    // Time complexity O(1)
+    // Spatial complexity O(1)
+    public T peek() {
+        if (head == null) { 
+            return null;
+        }
+        return head.value;
+    }
+
+    // Exercise No.3 
+    // result == null;
+}
