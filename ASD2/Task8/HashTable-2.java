849a828522f2c792e32a9f51041bd0608136a1cf
DenisG30 <gorbunowdenis30@gmail.com>
2025-10-21 09:07:01 +0300

    Update HashTable-2.java

--- ASD2/Task8/HashTable-2.java
+++ ASD2/Task8/HashTable-2.java
@@ -95,6 +95,7 @@ public class DynamicHashTable<K, V> {
 
 
 // Exercise No.4 
+
 public class MultiHashTable<K, V> {
     private static final int INITIAL_CAPACITY = 16;
     private static final double LOAD_FACTOR = 0.75;
@@ -195,6 +196,99 @@ public class MultiHashTable<K, V> {
 
 // Exercise No.6
  
+public class DDoSTest {
+    public static void main(String[] args) {
+        HashTable hashTable = new HashTable(10, 1); // Создаем хэш-таблицу размером 10
+
+        for (int i = 0; i < 100; i++) {
+            String key = generateCollisionKey(i);
+            hashTable.put(key);
+            System.out.println("Inserted: " + key);
+        }
+
+        for (int i = 0; i < hashTable.size; i++) {
+            System.out.println("Slot " + i + ": " + hashTable.slots[i]);
+        }
+    }
+
+    private static String generateCollisionKey(int index) {
+        return "a" + index;
+    }
+}
+
+
+public class HashTable {
+    public int size;
+    public int step; 
+    public String[] slots; 
+
+    public HashTable(int sz, int stp) {
+        size = sz;
+        step = stp;
+        slots = new String[size];
+        for (int i = 0; i < size; i++) slots[i] = null; 
+    }
+
+    public int customHashCode(String value) {
+        int hash = 7;
+        for (int i = 0; i < value.length(); i++) {
+            hash = hash * 31 + value.charAt(i);
+        }
+        return hash;
+    }
+
+    public int hashFun(String value) {
+        return (customHashCode(value) & 0x7FFFFFFF) % size; 
+    }
+
+    public int seekSlot(String value) {
+        int index = hashFun(value);
+        int originalIndex = index;
+
+        while (slots[index] != null) {
+            if (slots[index].equals(value)) {
+                return index; 
+            }
+            index = (index + step) % size; 
+            if (index == originalIndex) { 
+                break;
+            }
+        }
+
+        if (slots[index] == null) {
+            return index;
+        }
+        
+        return -1; 
+    }
+
+    public int put(String value) {
+        int slotIndex = seekSlot(value);
+       
+        if (slotIndex != -1 && slots[slotIndex] == null) { 
+            slots[slotIndex] = value;
+            return slotIndex;
+        }
+        
+        return -1;
+    }
+
+    public int find(String value) {
+        int index = hashFun(value);
+        int originalIndex = index;
+
+        while (slots[index] != null) {
+            if (slots[index].equals(value)) {
+                return index;
+            }
+            index = (index + step) % size;
+            if (index == originalIndex) {
+                break;
+            }
+        }
+        return -1;
+    }
+}
 
 
 // Рефлексия. 
