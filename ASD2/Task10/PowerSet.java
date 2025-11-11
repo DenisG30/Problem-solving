42e6a0d76abbf29b93c94c42e8b6b3db6fb2bbe5
DenisG30 <gorbunowdenis30@gmail.com>
2025-11-12 01:08:03 +0300

    Update PowerSet.java

--- ASD2/Task10/PowerSet.java
+++ ASD2/Task10/PowerSet.java
@@ -236,7 +236,7 @@ public class PowerSet
     }
 
 
-    public boolean equals(PowerSet set2)
+    public boolean equals(PowerSet<T> set2)
     {
         if(this.size != set2.size) {
             return false;
